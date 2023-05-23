fa278221226fce3ee7b3d5ca0b7f151b1604d9c2
kuriyantoadi <melatikuning212@gmail.com>
2023-05-23 10:45:16 +0700

    update 23 mei 2023, update 2

--- application/views/siswa/profil copy.php
+++ application/views/siswa/profil copy.php
@@ -91,14 +91,11 @@
             </div>
             <!-- end row data siswa-->
 
-            <!-- end row raport-->
             <div class="row">
                 <div class="col-12">
                     <div class="card">
-                        <div class="card-body text-center">
-                            <h4 class="mb-sm-0 font-size-18">Raport Semester 1</h4>
-                        </div>                                  
                         <div class="card-body">
+
                             <div class="row align-items-center">
                                 <div class="col-lg-4">
                                     <div class="card">
@@ -119,23 +116,23 @@
                                                             <tbody>
                                                                 <tr>
                                                                     <th>Agama</th>
-                                                                    <td><?= $row->sm_1_agama ?></td>
+                                                                    <td>90</td>
                                                                 </tr>
                                                                 <tr>
                                                                     <th>Matematika</th>
-                                                                    <td><?= $row->sm_1_mtk ?></td>
+                                                                    <td>90</td>
                                                                 </tr>
                                                                 <tr>
                                                                     <th>Bahasa Indonesia</th>
-                                                                    <td><?= $row->sm_1_bindo ?></td>
+                                                                    <td>90</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <th>Bahasa Inggris</th>
+                                                                    <td>90</td>
                                                                 </tr>
                                                                 <tr>
                                                                     <th>IPA</th>
-                                                                    <td><?= $row->sm_1_ipa ?></td>
-                                                                </tr>
-                                                                 <tr>
-                                                                    <th>Bahasa Inggris</th>
-                                                                    <td><?= $row->sm_1_bing ?></td>
+                                                                    <td>90</td>
                                                                 </tr>
                                                             </tbody>
                                                         </table>
@@ -146,6 +143,102 @@
                                         </div>
                                     </div>
                                     
+                                </div>
+                            </div>
+
+                            <div class="row align-items-center">
+                                <div class="col-lg-4">
+                                    <div class="card">
+                                        <div class="card-body text-center">
+                                            <h4 class="card-title">Raport Semester 2</h4>
+                                            <a href="#" type="button" class="btn btn-link waves-effect">(File PDF)</a>
+                                        </div>
+                                    </div> 
+                                </div>
+                                <div class="col-lg-8">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-md-8">
+                                                    <div class="table-responsive">
+                                                        <table class="table table-bordered border-1 table-sm">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <th>Agama</th>
+                                                                    <td>90</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <th>Matematika</th>
+                                                                    <td>90</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <th>Bahasa Indonesia</th>
+                                                                    <td>90</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <th>Bahasa Inggris</th>
+                                                                    <td>90</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <th>IPA</th>
+                                                                    <td>90</td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- <a href="#" class="btn btn-primary waves-effect waves-light btn-sm">Edit Nilai Raport</a> -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row align-items-center">
+                                <div class="col-lg-4">
+                                    <div class="card">
+                                        <div class="card-body text-center">
+                                            <h4 class="card-title">Raport Semester 2</h4>
+                                            <a href="#" type="button" class="btn btn-link waves-effect">(File PDF)</a>
+                                        </div>
+                                    </div> 
+                                </div>
+                                <div class="col-lg-8">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-md-8">
+                                                    <div class="table-responsive">
+                                                        <table class="table table-bordered border-1 table-sm">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <th>Agama</th>
+                                                                    <td>90</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <th>Matematika</th>
+                                                                    <td>90</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <th>Bahasa Indonesia</th>
+                                                                    <td>90</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <th>Bahasa Inggris</th>
+                                                                    <td>90</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <th>IPA</th>
+                                                                    <td>90</td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- <a href="#" class="btn btn-primary waves-effect waves-light btn-sm">Edit Nilai Raport</a> -->
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
 
